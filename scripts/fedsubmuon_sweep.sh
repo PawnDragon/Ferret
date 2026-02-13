9ecf725844f545518e5edd6a51237227b57fac41
Shaolong Chen <shaolong.chen25@imperial.ac.uk>
2026-02-13 14:34:49 +0000

    CHORE: modify sweep setting

--- scripts/fedsubmuon_sweep.sh
+++ scripts/fedsubmuon_sweep.sh
@@ -6,19 +6,16 @@ python main.py \
   --model /root/autodl-tmp/llama-3.2-1B/ \
   --dataset dolly \
   --K 200 \
-  -m 0.5 \
   --log \
-  --local_step 1 \
   --anneal no \
   --device 0 \
   --momentum 0.0 \
   --n_accum 1 \
   --equal_weight \
   --seed 494 \
-  --num_clients 10 \
   --seed_refresh_F 100 \
   --rounds 40 \
   --use_wandb \
   --wandb_project ferret \
-  --wandb_name sweep_lr_r \
+  --wandb_name sweep_lr_r_localstep \
   "$@"
